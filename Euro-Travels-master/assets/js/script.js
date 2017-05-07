af5f501481f79ec7f024cd627b46fa484b1ccb2d
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2017-05-07 10:24:43 +1000

    Change Logo

--- Euro-Travels-master/assets/js/script.js
+++ Euro-Travels-master/assets/js/script.js
@@ -16,9 +16,11 @@ $('a[href*=#]:not([href=#])').click(function() {
 
 
 $(document).ready(function(){
-    $('.about').click(function(e) {
-    $('.home').removeClass('active');
-});
+
+    $('.navbar-right li').click(function(){ 
+      $(this).siblings().removeClass('active');
+    }); 
+
     //googleMap
     $("#index-signup").click(function(e){
         setTimeout(function(){
